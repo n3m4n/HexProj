f7464d31b953609cc0229c5acc18b21c2bb54389
n3m4n <n3m4n@abv.bg>
2015-06-12 19:45:49 +0300

    First up

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Random = UnityEngine.Random;
+
+public class Mover : MonoBehaviour
+{
+    public Transform player;
+    public Sprite destroyed;
+    [HideInInspector]
+    public Vector3 currentPosition;
+    [HideInInspector]
+    public GameObject currentTile;
+
+    private Spawner spawner;
+
+    void Start()
+    {
+        spawner = GetComponent<Spawner>();
+        currentPosition = new Vector3((spawner.cols - 1) / 2, spawner.rowDifference, 0f);
+        player.position = currentPosition;
+    }
+
+
+    void Update()
+    {
+        player.position = Vector3.MoveTowards(player.position, currentPosition, 5 * Time.deltaTime);
+    }
+
+    public bool Move(Camera cam)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Vector3 hitPosition = hit.collider.transform.position;
+
+            if (hitPosition.x >= 0 && hitPosition.x <= spawner.cols - 1)
+                if (hitPosition.y - currentPosition.y <= spawner.rowDifference + 0.01f && hitPosition.y - currentPosition.y >= 0)
+                    if (Mathf.Abs(hitPosition.x - currentPosition.x) <= 1)
+                    {
+                        currentTile.GetComponent<Animator>().enabled = false;
+                        currentTile.GetComponent<SpriteRenderer>().sprite = destroyed;
+
+                        currentTile = hit.collider.gameObject;
+                        currentPosition = hitPosition;
+
+                        currentTile.GetComponent<Animator>().enabled = true;
+                        hit.collider.enabled = false;
+
+                        return true;
+                    }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour
+{
+    public const float oddRowDifference = 0.5f;
+
+    public static Spawner instance = null;
+    public float rowDifference;
+    public float camSpeed = 2.0f;
+    public int rows = 9;
+    public int cols = 7;
+    public GameObject[] tiles;
+    public Camera cam;
+
+    private bool oddRow = false;
+    private bool spawning = false;
+    private bool spawnTurn = false;
+    private int rowCount = 0;
+    private List<List<GameObject>> spawnedTiles;
+    private Mover mov;
+    private Vector3 newCamPosition;
+
+
+
+    void Start()
+    {
+        /*if (instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);*/
+
+        spawnedTiles = new List<List<GameObject>>();
+        mov = GetComponent<Mover>();
+        newCamPosition = cam.transform.position;
+        StartCoroutine(SpawnRow(rows, false));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+
+        /*if (Input.GetKeyDown(KeyCode.Alpha1)) { Application.LoadLevel(0); }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { Application.LoadLevel(1); }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { Application.LoadLevel(2); }*/
+
+        if (Input.GetKeyDown(KeyCode.R)) { Application.LoadLevel(Application.loadedLevel); }
+
+        if (Input.GetMouseButtonDown(0) && !spawning)
+        {
+            if (mov.Move(cam))
+            {
+                if (spawnTurn)
+                    SpawnNew();
+                spawnTurn = !spawnTurn ? true : false;
+            }
+        }
+        if (mov.currentPosition.y >= cam.transform.position.y + rowDifference && !spawning)
+        {
+            SpawnNew();
+        }
+
+        StartCoroutine(GameOver()); // WIP
+        cam.transform.position = Vector3.MoveTowards(cam.transform.position, newCamPosition, camSpeed * Time.deltaTime);
+    }
+
+    void SpawnNew()
+    {
+        newCamPosition += new Vector3(0f, rowDifference, 0f);
+
+        StartCoroutine(SpawnRow());
+
+        for (int i = 0; i < spawnedTiles[0].Count; i++)
+            Destroy(spawnedTiles[0][i].gameObject);
+        spawnedTiles.RemoveAt(0);
+    }
+
+    IEnumerator SpawnRow(int spawnCount = 1, bool wait = true)
+    {
+        spawning = true;
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            int count = oddRow ? cols : cols + 1;
+            List<GameObject> newHexagonRow = new List<GameObject>();
+
+            for (int j = 0; j < count; j++)
+            {
+                float xPos = j;
+                float yPos = rowCount * rowDifference;
+                if (!oddRow)
+                    xPos -= oddRowDifference;
+
+                GameObject instance = tiles[Random.Range(0, tiles.Length)];
+                instance = Instantiate(instance, new Vector3(xPos, yPos, 0f), Quaternion.identity) as GameObject;
+                newHexagonRow.Add(instance);
+
+                if (wait)
+                    yield return new WaitForSeconds(0.05f);
+            }
+
+            spawnedTiles.Add(newHexagonRow);
+            oddRow = !oddRow ? true : false;
+            rowCount++;
+
+            // WIP
+            if (i == 1)
+            {
+                mov.currentTile = spawnedTiles[i][2];
+                mov.currentTile.GetComponent<BoxCollider2D>().enabled = false;
+                mov.currentTile.GetComponent<Animator>().enabled = true;
+            }
+            // WIP
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        spawning = false;
+    }
+
+    IEnumerator GameOver()
+    {
+        if (cam.transform.position.y - cam.orthographicSize > mov.currentPosition.y)
+        {
+            spawning = true;
+            yield return new WaitForSeconds(1f);
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
